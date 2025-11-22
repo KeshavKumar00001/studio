f42880f3abd8ddd6a664ffafc3f958a1ffc78d13
Keshav Kumar <keshavkumar_bca23_26@its.edu.in>
2025-11-22 15:13:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1394267322-5b0dc",
+  "appId": "1:1099040046726:web:f1ec2bd475a5edab275b07",
+  "apiKey": "AIzaSyDXJQjYKEMLb5BovMlZyfSYZfHyLtyHyIs",
+  "authDomain": "studio-1394267322-5b0dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1099040046726"
+};
